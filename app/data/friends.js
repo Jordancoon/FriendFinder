9b8cbd50c3be8141c23c07f3cea5e7e3dee0cdaf
jordan <jgcoon24@gmail.com>
2018-10-04 18:20:16 -0500

    add images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 var friends = [
     {
       "name" : "Darth Vader",
-      "photo" : "https://jordancoon.github.io/FriendFinder/hostedimages/cthulhu.jpg",
+      "photo" : "https://jordancoon.github.io/FriendFinder/hostedimages/vader.jpg",
       "scores" : [5, 5, 5, 5, 5, 5, 5]
     },
     {
       "name" : "Yoda",
-      "photo" : "https://jordancoon.github.io/FriendFinder/hostedimages/homer.jpg",
+      "photo" : "https://jordancoon.github.io/FriendFinder/hostedimages/yoda.jpg",
       "scores" : [3, 3, 3, 3, 3, 3, 3]
     },
     {
       "name" : "Han Solo",
-      "photo" : "https://jordancoon.github.io/FriendFinder/hostedimages/ned.jpg",
+      "photo" : "https://jordancoon.github.io/FriendFinder/hostedimages/hansolo.jpg",
       "scores" : [1, 1, 1, 1, 1, 1, 1]
     }
   ];
