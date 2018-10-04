73c6c79023bbde25be35a89b58c3c64bbdcdfc80
jordan <jgcoon24@gmail.com>
2018-10-04 18:30:04 -0500

    final changes

--- app/data/questions.js
+++ app/data/questions.js
@@ -1,31 +1,31 @@
 var questions = [
     {
       "id"   : 1,
-      "text" : "Post-apocalyptic visions of the future always put a smile on your face."
+      "text" : "using the deathstar to detroy planets brings a smile to your face."
     },
     {
       "id"   : 2,
-      "text" : "When choosing friends, your first thought is, \"I wonder what they would taste like barbecued...\""
+      "text" : "When choosing friends, your first thought is, we need a wild card."
     },
     {
       "id"   : 3,
-      "text" : "On a Sunday night, you'd prefer performing a ritual sacrifice over watching Game of Thrones."
+      "text" : "On a Sunday night, you'd prefer training and lifting objects."
     },
     {
       "id"   : 4,
-      "text" : "All of your friends belong to the same cult."
+      "text" : "All of your friends belong to the same group."
     },
     {
       "id"   : 5,
-      "text" : "Cthulhu will make a fantastic overlord, and you look forward to serving him."
+      "text" : "CVader will make a fantastic overlord, and you look forward to serving him."
     },
     {
       "id"   : 6,
-      "text" : "Your idea of a good time is being annhilated in the fiery fury of Cthulhu's impending rage."
+      "text" : "Your idea of a good time is being annhilated the resistence scum!"
     },
     {
       "id"   : 7,
-      "text" : "You're excited to vote for Cthulhu in the 2020 U.S. presidential election."
+      "text" : "Lord Vader over everything!"
     }
   ];
   
